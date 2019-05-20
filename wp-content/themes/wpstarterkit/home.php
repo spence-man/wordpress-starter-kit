24d04f8577e41d30271cbdf73cb06a3b3eb4c818
spence-man <spencer.mandzik@gmail.com>
2019-05-20 15:16:13 -0700

    enable bootstrap js & home page welcome

--- wp-content/themes/wpstarterkit/home.php
+++ wp-content/themes/wpstarterkit/home.php
@@ -5,6 +5,51 @@ Template Name: Home
 get_header();
 ?>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+<a class="navbar-brand" href="#">
+    <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Bootstrap
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+    </ul>
+</div>
+
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Welcome to the Wordpress Starter Kit</h1>
+        <p class="lead">This is a basic theme using the wordpress starter kit repo for WordPress local development testing.</p>
+        <hr class="my-4">
+        <p>This is a demo page with bootstrap loaded and some super basic default theme files</p>
+        <a class="btn btn-primary btn-lg" href="https://github.com/spence-man/wordpress-starter-kit" role="button" target="_blank" >Learn more</a>
+    </div>
+</div>
+
+<div class="container">
+
 <?php if (have_posts()) :
     while (have_posts()) :
         the_post();?>
@@ -15,4 +60,5 @@ get_header();
     <?php endwhile; ?>
 <?php endif;?>
 
+</div>
 <?php get_footer();
